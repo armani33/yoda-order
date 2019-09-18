594c3020e6728137eb4ff3e9b4779b0a17c07718
armand chariot <armand@chariot.be>
2019-09-18 10:33:00 +0200

    add function for phone view in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,32 @@
 class PagesController < ApplicationController
+  before_action :set_variant, only: [:main]
   def main
+
+    respond_to do |format|
+      format.html(&:phone)
+      # do |html|
+      # html.phone
+      # html.tablet
+      # end
+    end
   end
+
+
+  private
+
+  def set_variant
+    case request.user_agent
+    when /iPhone/i
+      request.variant = :phone
+    when /Android/i && /mobile/i
+      request.variant = :phone
+    when /Windows Phone/i
+      request.variant = :phone
+    # when /Android/i
+    #   request.variant = :tablet
+    # when /iPad/i
+    #   request.variant = :tablet
+    end
+  end
+
 end
