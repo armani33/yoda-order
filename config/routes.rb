2201f01902d1dde4b8172c556f4eae8993f073df
armand chariot <armand@chariot.be>
2019-09-28 17:57:11 +0200

    done with the review manage backend + styling, still not finish cause need to add devise security

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,16 @@ Rails.application.routes.draw do
   get 'app' => 'pages#app'
   get 'reviews' => 'pages#reviews'
 
+
   get 'newsletter/buy' => 'users#new_buy'
   get 'newsletter/test' => 'users#new_test'
   post 'users/create' => 'users#create'
 
-  get 'reviews/new' => 'reviews#new'
-  post 'reviews/create' => 'reviews#create'
+
+  get 'reviews/manage/new' => 'reviews#new'
+  post 'reviews/manage/create' => 'reviews#create'
+  get 'reviews/manage' => 'reviews#manage'
+  resources :reviews, only: [:edit, :update, :destroy]
+
 
 end
