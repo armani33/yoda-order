633e3b4840645f484134992ef53d779bfb322e02
armand chariot <armand@chariot.be>
2019-10-23 11:15:55 +0200

    change the production default url mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :postmark
   config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: 'www.yoda-city.com' }
+  config.action_mailer.default_url_options = { host: 'yoda-city.com' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
