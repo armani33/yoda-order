80a7e5cd88e7418af9207e9aed39fed9198b873e
armand chariot <armand@chariot.be>
2019-10-23 13:03:21 +0200

    try to use smtp in env production for postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,9 +65,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_KEY'] }
-  config.action_mailer.default_url_options = { host: 'yoda-city.com' }
+  config.action_mailer.default_url_options = { :host => 'yoda-city.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.postmarkapp.com',
+    port:                 587,
+    domain:               'yoda-city.com',
+    username:            ENV["POSTMARK_API_KEY"],
+    password:             ENV["POSTMARK_API_KEY"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
