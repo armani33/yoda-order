b8c8401e3cb703e71446c7af09802d744d28f5f3
armand chariot <armand@chariot.be>
2019-11-25 15:30:07 +0100

    delete unecessary migration files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root to: 'pages#main'
 
   get 'rent-a-yoda' => 'pages#rent'
--- db/migrate/20190927114506_change_column_name_in_reviews.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameInReviews < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :reviews, :available_time, :datum
-  end
-end
