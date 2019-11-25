6f31f72364fc3d89237ddc8be0881b0430b94ad2
armand chariot <armand@chariot.be>
2019-11-25 15:42:04 +0100

    make some validates to User modelé

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+
+  validates :email, presence: true, uniqueness: true, format: {
+    with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/i,
+    message: 'Invalid email format.'
+  }
+
+  before_create :downcase_email
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
