bf37937a4d6ff41b1568a172f91cff89aeec9cad
armand chariot <armand@chariot.be>
2019-11-26 21:35:29 +0100

    begin to style the refer-friend page

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,36 @@ class User < ApplicationRecord
   before_create :create_referral_code
   before_create :downcase_email
 
+  REFERRAL_STEPS = [
+    {
+      'count' => 5,
+      'html' => '10 Rides<br>Free',
+      'class' => 'two',
+      # 'image' =>  ActionController::Base.helpers.asset_path(
+      #   'refer/cream-tooltip@2x.png')
+    },
+    {
+      'count' => 10,
+      'html' => '20 Rides<br> + 30 minutes Free',
+      'class' => 'three',
+      # 'image' => ActionController::Base.helpers.asset_path(
+      #   'refer/truman@2x.png')
+    },
+    {
+      'count' => 25,
+      'html' => '40 Rides<br> + 80 minutes Free<br>+ 1 T-shirt',
+      'class' => 'four',
+      # 'image' => ActionController::Base.helpers.image_path("reduc_3.png")
+    },
+    {
+      'count' => 50,
+      'html' => '100 Rides<br> + 200 minutes Free<br>+ 1 T-shirt',
+      'class' => 'five',
+      # 'image' => ActionController::Base.helpers.asset_path(
+      #   'refer/blade-explain@2x.png')
+    }
+  ]
+
   private
 
   def downcase_email
